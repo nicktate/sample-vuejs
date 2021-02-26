9f2284608908adf4d8e9da72a67f2ad9f4b7c1eb
Nicholas Tate <ntate@digitalocean.com>
2021-02-26 12:26:24 -0800

    wip

--- src/main.js
+++ src/main.js
@@ -7,3 +7,10 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+console.log("NICK: ", process.env.NICK_VAR)
+console.log("NICK EXPAND: ", process.env.NICK_VAR_EXPAND)
+console.log("NICK PREEXIST: ", process.env.NICK_VAR_PRE)
+console.log("invalidate cache")
+console.log("VUE NICK: ", process.env.VUE_APP_NICK_VAR)
+console.log("VUE NICK EXPAND: ", process.env.VUE_APP_EXPAND)
